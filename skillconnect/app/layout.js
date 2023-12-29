79bf255be3cc3e70817ee0e4f7ff15ba85627878
Shashank Phatkure <shashankphatkure@gmail.com>
2023-12-30 01:23:04 +0530

    updated header and footer for pages

--- skillconnect/app/layout.js
+++ skillconnect/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Footers1 from '@/components/footers/Footers1'
+import Headers1 from '@/components/headers/Headers1'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Headers1/>
       <div>{children}</div>
       <Footers1 />
       </body>
