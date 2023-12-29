fda2be9fd9a95d74140d9053192dc4fb45257e31
Shashank Phatkure <shashankphatkure@gmail.com>
2023-12-30 02:22:36 +0530

    added dashboard and shop pages and other important pages like pricing and 404

--- skillconnect/app/page.js
+++ skillconnect/app/page.js
@@ -1,12 +1,12 @@
 import BlogSectionBlogs from '@/components/blog-sections/BlogSectionBlogs'
 import BlogSectionCourse from '@/components/blog-sections/BlogSectionCourse'
 import BlogSectionJobs from '@/components/blog-sections/BlogSectionJobs'
-import CardheadingsInternship from '@/components/card-headings/CardheadingsInternship'
 import Categorypreviews1 from '@/components/category-previews/Categorypreviews1'
 import Dividers1 from '@/components/dividers/Dividers1'
 import Heroes1 from '@/components/heroes/Heroes1'
 import LogoClouds1 from '@/components/logo-clouds/LogoClouds1'
 import Testimonials1 from '@/components/testimonials/Testimonials1'
+import './globals.css'
 
 export default function Home() {
   return (
