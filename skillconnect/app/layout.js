05ae9c31d2be3164b1aae3c2e939f59077d749a3
Shashank Phatkure <shashankphatkure@gmail.com>
2023-12-30 00:11:47 +0530

    added homepage components

--- skillconnect/app/layout.js
+++ skillconnect/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Footers1 from '@/components/footers/Footers1'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div>{children}</div>
+      <Footers1 />
+      </body>
     </html>
   )
 }
