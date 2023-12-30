01dab5469de22ceed9fe00084c6b485a1a23fdf5
Shashank Phatkure <shashankphatkure@gmail.com>
2023-12-30 18:23:12 +0530

    added more components in webapp

---
+++ skillconnect/app/dashboard/courselayout/page.jsx
@@ -0,0 +1,12 @@
+import Applicationshell1 from '@/components/application-shells/Applicationshell1'
+import React from 'react'
+
+const courselayout = () => {
+    return (
+        <div>
+        <Applicationshell1 />
+        </div>
+    )
+}
+
+export default courselayout
---
+++ skillconnect/components/CourseVideo/Coursevideo1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Coursevideo1 = () => {
+    return (
+        <div>
+            <iframe 
+                width="100%" 
+                height="500" 
+                src="https://www.youtube.com/embed/zlX0lrX2oLA?si=iEJo05BIR8DJfr3t" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen
+            ></iframe>
+        </div>
+    )
+}
+
+export default Coursevideo1
