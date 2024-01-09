ddb16f8c6d89c1d125c520681846b5ddc69253b8
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-09 18:34:48 +0530

    added web analytics

--- skillconnect/app/layout.js
+++ skillconnect/app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       <div>{children}</div>
       <Footers1 />
       <SpeedInsights />
+      <Analytics />
       </body>
     </html>
   )
