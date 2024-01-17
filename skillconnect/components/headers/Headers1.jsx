b1b967dc924a9344135029e729427b91f0b1528c
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-17 20:43:08 +0530

    added dark mode ( test )

--- skillconnect/components/headers/Headers1.jsx
+++ skillconnect/components/headers/Headers1.jsx
@@ -149,7 +149,7 @@ export default function Headers1() {
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
+            Dark Mode <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
         <div>
