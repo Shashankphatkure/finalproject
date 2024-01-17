c23fd6a4e5d891201c910f880b2be76b9e2a363f
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-17 17:41:38 +0530

    minor updates

--- skillconnect/next.config.js
+++ skillconnect/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-
+const isProd = process.env.NODE_ENV === 'production'
 
 const withPWA = require('next-pwa')({
     dest: 'public',
     register: true,
     skipWaiting: true,
+    disable: !isProd,
 })
 
 const nextConfig = {}
