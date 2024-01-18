c9f65a2689aa262a783e2974efd8f67d6ef3f5db
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-19 02:48:06 +0530

    added instructor dashboard

---
+++ skillconnect/components/dashboard-headers/DashboardheadersInstructor.jsx
@@ -0,0 +1,130 @@
+"use client"
+import Coursecard from '@/components/Important-cards/Coursecard'
+
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Coursesearch from '@/components/Searchdashboard/coursesearch'
+
+const navigation = [
+  { name: 'Created Courses', href: '#', current: true },
+  { name: 'Revenue', href: '#', current: false },
+  { name: 'Sales', href: '#', current: false },
+  { name: 'Share', href: '#', current: false },
+  { name: 'Community', href: '#', current: false },
+]
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function DashboardHeaderInstructor({ children }) {
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+      <div className="min-h-full">
+        <div className="bg-indigo-600 pb-32">
+          <Disclosure as="nav" className="border-b border-indigo-300 border-opacity-25 bg-indigo-600 lg:border-none">
+            {({ open }) => (
+              <>
+                <div className="mx-auto max-w-7xl sm:px-4 lg:px-8">
+                  <div className="relative flex h-16 items-center justify-between lg:border-b lg:border-indigo-400 lg:border-opacity-25">
+                    <div>
+                      <div>
+                        <div className="flex space-x-4">
+                          {navigation.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className={classNames(
+                                item.current
+                                  ? 'bg-indigo-700 text-white'
+                                  : 'text-white hover:bg-indigo-500 hover:bg-opacity-75',
+                                'rounded-md py-2 px-3 text-sm font-medium'
+                              )}
+                              aria-current={item.current ? 'page' : undefined}
+                            >
+                              {item.name}
+                            </a>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                    <Coursesearch/>
+                    <div className="flex lg:hidden">
+                      {/* Mobile menu button */}
+                      <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-indigo-600 p-2 text-indigo-200 hover:bg-indigo-500 hover:bg-opacity-75 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600">
+                        <span className="absolute -inset-0.5" />
+                        <span className="sr-only">Open main menu</span>
+                        {open ? (
+                          <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                        ) : (
+                          <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                        )}
+                      </Disclosure.Button>
+                    </div>
+                    <div>
+                      <div className="flex items-center">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <Disclosure.Panel className="lg:hidden">
+                  <div className="space-y-1 px-2 pb-3 pt-2">
+                    {navigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-indigo-700 text-white'
+                            : 'text-white hover:bg-indigo-500 hover:bg-opacity-75',
+                          'block rounded-md py-2 px-3 text-base font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
+                  
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+          <header className="py-10">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <h1 className="text-3xl font-bold tracking-tight text-white">Enrolled Courses</h1>
+            </div>
+          </header>
+        </div>
+        {children}
+      </div>
+    </>
+  )
+}
