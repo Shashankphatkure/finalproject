a776ba6e6976d839f0dee810053f215d22bdba4d
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-19 02:32:04 +0530

    debug

--- skillconnect/app/course/instructor/page.js
+++ skillconnect/app/course/instructor/page.js
@@ -4,6 +4,7 @@ import Datadisplay1 from '@/components/data-displays/data-display1'
 import Testimonials1 from '@/components/testimonials/Testimonials1'
 import Cta1 from '@/components/CTA Sections/cta1'
 import Cta2 from '@/components/CTA Sections/cta2'
+import SectionHeader1 from '@/components/headers/section-header1'
 
 const page = () => {
   return (
