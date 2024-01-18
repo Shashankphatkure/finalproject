aeb2a6223b7a5d001088e4a728af18d41024dddc
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-19 04:15:48 +0530

    updated search page for jobs and courses

--- skillconnect/components/Important-cards/Coursecard.jsx
+++ skillconnect/components/Important-cards/Coursecard.jsx
@@ -9,14 +9,10 @@ export default async function Page() {
   const data = await getData()
   
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:max-w-4xl">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Latest Courses</h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
-          </p>
-          <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+          <div className="mt-4 space-y-10 lg:space-y-10">
             {data.map((item) => (
               <article key={item.id} className="relative isolate flex flex-col gap-8 lg:flex-row">
                 <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
--- skillconnect/components/blog-sections/BlogSectionCourse.jsx
+++ skillconnect/components/blog-sections/BlogSectionCourse.jsx
@@ -61,7 +61,7 @@ const posts = [
 
 export default function BlogSectionCourse() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-8 sm:py-8">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Featured Courses</h2>
