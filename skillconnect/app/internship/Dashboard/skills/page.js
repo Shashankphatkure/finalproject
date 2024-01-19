eed794cc1c64d4b7692f77792f4bdbb2fcbf3002
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-19 17:31:45 +0530

    improved folder structure

---
+++ skillconnect/app/internship/Dashboard/skills/page.js
@@ -0,0 +1,16 @@
+import Coursecard from '@/components/Important-cards/Coursecard'
+import BlogSectionCourse from '@/components/blog-sections/BlogSectionCourse'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <BlogSectionCourse />
+      <Coursecard />
+    </div>
+  )
+}
+
+export default page
+
+
