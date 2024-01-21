85999952447695f5f566807412cb90538d5e822f
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-21 18:21:03 +0530

    debug

--- skillconnect/app/api/auth/[auth0]/route.js
+++ skillconnect/app/api/auth/[auth0]/route.js
@@ -1,11 +1,7 @@
-import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
     login: handleLogin({
         returnTo: "/auth/userprofile",
       }),
-    logout: handleLogout({
-        returnTo: "/logaaa",
-      }),  
 });
-
