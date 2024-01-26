0ca8809f12c21ef1a0988d1201633b89a0e41940
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-27 00:59:28 +0530

    updated events card with data

--- skillconnect/components/Important-cards/Eventcard.jsx
+++ skillconnect/components/Important-cards/Eventcard.jsx
@@ -2,7 +2,7 @@ import Bookmark from "../buttons/bookmark"
 import BuyButton from "../buttons/buybutton"
 
 async function getData() {
-  const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/courses?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
+  const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/events?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
   return res.json()
 }
 
@@ -20,7 +20,7 @@ export default async function Eventcard() {
               <article key={item.id} className="relative isolate flex flex-col gap-8 lg:flex-row">
                 <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
                   <img
-                    src={item.imageUrl}
+                    src={item.thumbnail}
                     alt=""
                     className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
                   />
@@ -28,8 +28,8 @@ export default async function Eventcard() {
                 </div>
                 <div>
                   <div className="flex items-center gap-x-4 text-xs">
-                    <time dateTime={item.updateddate} className="text-gray-500">
-                      {item.updateddate}
+                    <time dateTime={item.date_and_time} className="text-gray-500">
+                      {item.date_and_time}
                     </time>
                     <a
                       className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
@@ -53,10 +53,10 @@ export default async function Eventcard() {
                         <p className="font-semibold text-gray-900">
                           <a>
                             <span className="absolute inset-0" />
-                            Hosted by {item.author}
+                            Hosted by {item.authorid}
                           </a>
                         </p>
-                        <p className="text-gray-600">Location : {item.author}</p>
+                        <p className="text-gray-600">Location : {item.location}</p>
                       </div>
                       <Bookmark text="Reserve Spot"/>
                     </div>
