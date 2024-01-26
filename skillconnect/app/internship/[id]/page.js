30a2e218b2d7ca84b39031da8d6dcc059c731f86
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-27 05:20:52 +0530

    updated

--- skillconnect/app/internship/[id]/page.js
+++ skillconnect/app/internship/[id]/page.js
@@ -1,3 +1,5 @@
+
+
 import BlogSectionReviews from "@/components/blog-sections/BlogSectionReviews"
 import Datadisplay1 from "@/components/data-displays/data-display1"
 import DescriptionList from "@/components/descriptionlist/page"
@@ -5,26 +7,25 @@ import Benefitsperks from "@/components/lists/benefitsperks"
 import Jobslist from "@/components/lists/jobslist"
 import Pageheadings2 from "@/components/page-headings/pageheadings2"
 
-// export async function generateStaticParams() {
-//     const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/internships?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
-//     const data = await res.json()
+export async function generateStaticParams() {
+    const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/internships?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
+    const data = await res.json()
    
-//     return data.map((course) => ({
-//       id: data.id,
-//     }))
-//   }
+    return data.map((course) => ({
+      id: data.id,
+    }))
+  }
   
-//   async function getData(id) {
-//     const res = await fetch(`https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/internships?id=eq.${id}&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY`, { cache: 'no-store' })
-//     const data = await res.json()
-//     return data
-//   }
+  async function getData(id) {
+    const res = await fetch(`https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/internships?id=eq.${id}&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY`, { cache: 'no-store' })
+    const data = await res.json()
+    return data
+  }
     
   export default async function Page({ params }) {
     const landingcourse = await getData(params.id)
     
-    return (
-    <div>
+    return (<div>
             {landingcourse.map((item) =>  (
                <div key={item.id}>
               <div>
@@ -38,14 +39,14 @@ import Pageheadings2 from "@/components/page-headings/pageheadings2"
         </main>
 
         <aside className="flex-1 sticky top-8 hidden w-96 shrink-0 xl:block">{/* Right column area */}</aside>
-        {/* <div>
+        <div>
         <Benefitsperks />
         <div>
         <Datadisplay1 />
         </div>
         <div className="mt-6" />
         <Jobslist />
-        </div> */}
+        </div>
          
       </div>
       <div>
