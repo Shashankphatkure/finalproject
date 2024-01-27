b3c5f42b11ba338feda5180635573b44f72bdf80
Shashank Phatkure <shashankphatkure@gmail.com>
2024-01-27 23:15:18 +0530

    debug

--- skillconnect/app/blogs/[id]/page.js
+++ skillconnect/app/blogs/[id]/page.js
@@ -1,5 +1,5 @@
 export async function generateStaticParams() {
-  const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/courses?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
+  const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/blogs?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
   const data = await res.json()
  
   return data.map((course) => ({
@@ -8,7 +8,7 @@ export async function generateStaticParams() {
 }
 
 async function getData(id) {
-  const res = await fetch(`https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/courses?id=eq.${id}&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY`)
+  const res = await fetch(`https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/blogs?id=eq.${id}&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY`)
   const data = await res.json()
   return data
 }
@@ -23,11 +23,7 @@ export default async function Page({ params }) {
              <div key={item.id}>
 
              <h3>Title : {item.title}</h3>
-             <p>Author Name : {item.author}</p>
-             <h3>Price : {item.price}</h3>
-             <p>Duration : {item.duration}</p>
              <p>Description : {item.description}</p>
-            <p>difficulty : {item.difficulty}</p>
             <div>Number: {item.id}</div>
             
             <br/>
