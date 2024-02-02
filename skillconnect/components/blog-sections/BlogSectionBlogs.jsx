bfd4594431350605c079f442e588a81b645827c1
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-02 17:25:18 +0530

    updated home

--- skillconnect/components/blog-sections/BlogSectionBlogs.jsx
+++ skillconnect/components/blog-sections/BlogSectionBlogs.jsx
@@ -1,3 +1,8 @@
+async function getData() {
+  const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/blogs?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
+  return res.json()
+}
+
 const posts = [
   {
     id: 1,
@@ -50,7 +55,8 @@ const posts = [
   // More posts...
 ]
 
-export default function BlogSectionBlogs() {
+export default async function BlogSectionBlogs() {
+  const posts = await getData()
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -66,22 +72,18 @@ export default function BlogSectionBlogs() {
               key={post.id}
               className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
             >
-              <img src={post.imageUrl} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
+              <img src={post.thumbnail} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
               <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
               <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
 
               <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-                <time dateTime={post.datetime} className="mr-8">
-                  {post.date}
+                <time dateTime={post.updatedate} className="mr-8">
+                  {post.updatedate}
                 </time>
                 <div className="-ml-4 flex items-center gap-x-4">
                   <svg viewBox="0 0 2 2" className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
                     <circle cx={1} cy={1} r={1} />
                   </svg>
-                  <div className="flex gap-x-2.5">
-                    <img src={post.author.imageUrl} alt="" className="h-6 w-6 flex-none rounded-full bg-white/10" />
-                    {post.author.name}
-                  </div>
                 </div>
               </div>
               <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
