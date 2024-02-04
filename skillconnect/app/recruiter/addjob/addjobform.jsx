178e4f9cc930321bf44150c5cc1dcb306cd7a736
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-04 20:20:37 +0530

    updated for skills and arrays

--- skillconnect/app/recruiter/addjob/addjobform.jsx
+++ skillconnect/app/recruiter/addjob/addjobform.jsx
@@ -12,6 +12,9 @@ const Addjobform = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    // Convert skills to array
+    const skillsArray = skills.value.split(',');
+
     // Use Supabase client to insert data
     const { data, error } = await supabase
       .from('jobs')
@@ -24,6 +27,7 @@ const Addjobform = () => {
           salary: salary.value,
           jobtype: jobtype.value,
           openings: openings.value,
+          skills: skillsArray,
         },
       ]);
 
@@ -37,17 +41,17 @@ const Addjobform = () => {
 
 
     return (
-    <div class="relative">
+    <div className="relative">
 
-      <div class="flex flex-col border rounded-xl p-4 sm:p-6 lg:p-10 dark:border-gray-700">
-        <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200">
+      <div className="flex flex-col border rounded-xl p-4 sm:p-6 lg:p-10 dark:border-gray-700">
+        <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">
           Fill in the form
         </h2>
 
         <form onSubmit={handleSubmit}>
-          <div class="mt-6 grid gap-4 lg:gap-6">
+          <div className="mt-6 grid gap-4 lg:gap-6">
             
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
               <div>
                 <label for="hs-firstname-hire-us-1" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">Title</label>
                 <input type="text" name="title" id="title" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"></input>
@@ -76,7 +80,7 @@ const Addjobform = () => {
               </div>
 
           
-            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
               <div>
                 <label for="hs-company-hire-us-1" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">Openings</label>
                 <input type="text" name="openings" id="openings" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"></input>
@@ -85,6 +89,11 @@ const Addjobform = () => {
               <div>
                 <label for="hs-lastname-hire-us-1" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">Salary</label>
                 <input type="text" name="salary" id="salary" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"></input>
+              </div>
+
+              <div>
+                <label for="hs-lastname-hire-us-1" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">skills</label>
+                <input type="text" name="skills" id="skills" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"></input>
               </div>
 
 
@@ -99,20 +108,20 @@ const Addjobform = () => {
          
 
          
-          <div class="mt-3 flex">
-            <div class="ms-3">
-              <label for="remember-me" class="text-sm text-gray-600 dark:text-gray-400">By submitting this form I have read and acknowledged the <a class="text-blue-600 decoration-2 hover:underline font-medium" href="#">Privacy policy</a></label>
+          <div className="mt-3 flex">
+            <div className="ms-3">
+              <label for="remember-me" className="text-sm text-gray-600 dark:text-gray-400">By submitting this form I have read and acknowledged the <a className="text-blue-600 decoration-2 hover:underline font-medium" href="#">Privacy policy</a></label>
             </div>
           </div>
           
 
-          <div class="mt-6 grid">
-            <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Send inquiry</button>
+          <div className="mt-6 grid">
+            <button type="submit" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Send inquiry</button>
           </div>
         </form>
 
-        <div class="mt-3 text-center">
-          <p class="text-sm text-gray-500">
+        <div className="mt-3 text-center">
+          <p className="text-sm text-gray-500">
             We'll get back to you in 1-2 business days.
           </p>
         </div>
