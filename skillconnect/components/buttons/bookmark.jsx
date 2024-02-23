ab1f6eb5eb3f25ffd398effe2c091443ec310008
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-24 00:05:45 +0530

    refactored course dynamic landing page

--- skillconnect/components/buttons/bookmark.jsx
+++ skillconnect/components/buttons/bookmark.jsx
@@ -8,7 +8,7 @@ export default function Bookmark({ text }) {
         className="relative inline-flex items-center gap-x-1.5 rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
       >
         <CheckIcon className="-ml-0.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-        {text}
+        BUY
       </button>
       <button
         type="button"
