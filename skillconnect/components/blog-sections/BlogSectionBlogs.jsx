941aa7cd7b8b487d249357ffbfe4bf79b15837a9
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-23 19:37:57 +0530

    top recommendations updated

--- skillconnect/components/blog-sections/BlogSectionBlogs.jsx
+++ skillconnect/components/blog-sections/BlogSectionBlogs.jsx
@@ -1,98 +1,91 @@
+import Link from "next/link"
+import Bookmark from "../buttons/bookmark"
+
+import { createClient } from '@supabase/supabase-js'
+
+// Initialize Supabase client
+const supabaseUrl = 'https://dxdpmgjttftkiqtlgcng.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY'
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
 async function getData() {
-  const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/blogs?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
-  return res.json()
+  // Fetch data from the blogs table
+  const { data, error } = await supabase
+    .from('blogs')
+    .select(`
+    *,
+    user:users (
+      username,
+      imageUrlAuthor,
+      role
+    )
+  `)
+  .limit(3)
+
+  if (error) {
+    console.error('Error fetching data:', error)
+    return []
+  }
+
+  return data
 }
 
-const posts = [
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    author: {
-      name: 'Michael Foster',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 2,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    author: {
-      name: 'Michael Foster',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    id: 3,
-    title: 'Boost your conversion rate',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    author: {
-      name: 'Michael Foster',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  // More posts...
-]
-
-export default async function BlogSectionBlogs() {
-  const posts = await getData()
+export default async function BlogSectionCourse() {
+  const data = await getData()
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-8 sm:py-8">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Unlock Your Potential</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
+          Dive into insightful articles, tutorials, and guides designed to elevate your skills
           </p>
         </div>
-        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
-            <article
-              key={post.id}
-              className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
-            >
-              <img src={post.thumbnail} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
-              <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-              <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-
-              <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-                <div className="-ml-4 flex items-center gap-x-4">
-                  <svg viewBox="0 0 2 2" className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
-                    <circle cx={1} cy={1} r={1} />
-                  </svg>
-                  <div className="flex gap-x-2.5">
-                    <img src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" className="h-6 w-6 flex-none rounded-full bg-white/10" />
-                    By Skillconnect Team
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {data.map((post) => (
+            <article key={post.id} className="flex flex-col items-start justify-between">
+              <div className="relative w-full">
+                <img
+                  src={post.thumbnail}
+                  alt=""
+                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                />
+                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+              </div>
+              <div className="max-w-xl">
+                <div className="mt-8 flex items-center gap-x-4 text-xs">
+                  <time dateTime={post.updateddate} className="text-gray-500">
+                    {post.updateddate}
+                  </time>
+                  <a
+                    href={post.category.href}
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    {post.category}
+                  </a>
+                </div>
+                <div className="group relative">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a href={post.href}>
+                      <span className="absolute inset-0" />
+                      {post.title}
+                    </a>
+                  </h3>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+                </div>
+                <div className="relative mt-8 flex items-center gap-x-4">
+                  <img src={post.user.imageUrlAuthor} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
+                  <div className="text-sm leading-6">
+                    <p className="font-semibold text-gray-900">
+                      <a href={post.author.href}>
+                        <span className="absolute inset-0" />
+                        {post.user.username}
+                      </a>
+                    </p>
+                    <p className="text-gray-600">{post.user.role}</p>
                   </div>
                 </div>
               </div>
-              <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
-                <a href={post.href}>
-                  <span className="absolute inset-0" />
-                  {post.title}
-                </a>
-              </h3>
             </article>
           ))}
         </div>
--- skillconnect/components/blog-sections/BlogSectionJobs.jsx
+++ skillconnect/components/blog-sections/BlogSectionJobs.jsx
@@ -1,63 +1,93 @@
+import Link from "next/link"
+import Bookmark from "../buttons/bookmark"
+
+import { createClient } from '@supabase/supabase-js'
+
+// Initialize Supabase client
+const supabaseUrl = 'https://dxdpmgjttftkiqtlgcng.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY'
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
 async function getData() {
-  const res = await fetch('https://dxdpmgjttftkiqtlgcng.supabase.co/rest/v1/jobs?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4ZHBtZ2p0dGZ0a2lxdGxnY25nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNzk5NDQsImV4cCI6MjAxNDg1NTk0NH0.DHTq4WkHgys5v0D9dj4i9Vfc9TCF7VuiGvRGR5RXYIY', { cache: 'no-store' })
-  const data = await res.json()
-  return data.slice(0, 3)
+  // Fetch data from the blogs table
+  const { data, error } = await supabase
+    .from('blogs')
+    .select(`
+    *,
+    user:users (
+      username,
+      imageUrlAuthor,
+      role
+    )
+  `)
+  .limit(3)
+
+  if (error) {
+    console.error('Error fetching data:', error)
+    return []
+  }
+
+  return data
 }
 
-export default async function BlogSectionJobs() {
-  const jobOpenings = await getData()
-
+export default async function BlogSectionCourse() {
+  const data = await getData()
   return (
-    <div className="bg-white py-32">
+    <div className="bg-white py-8 sm:py-8">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
-          <div className="w-full lg:max-w-lg lg:flex-auto">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Find your dream job
-            </h2>
-            <p className="mt-6 text-xl leading-8 text-gray-600">
-            Search for jobs by keyword, location, or company.
-            </p>
-            <img
-              src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&h=1104&q=80"
-              alt=""
-              className="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]"
-            />
-          </div>
-          <div className="w-full lg:max-w-xl lg:flex-auto">
-            <h3 className="sr-only">Job openings</h3>
-            <ul className="-my-8 divide-y divide-gray-100">
-              {jobOpenings.map((opening) => (
-                <li key={opening.id} className="py-8">
-                  <dl className="relative flex flex-wrap gap-x-3">
-                    <dt className="sr-only">Role</dt>
-                    <dd className="w-full flex-none text-lg font-semibold tracking-tight text-gray-900">
-                      <a href={opening.href}>
-                        {opening.title}
-                        <span className="absolute inset-0" aria-hidden="true" />
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Unlock Your Potential</h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+          Dive into insightful articles, tutorials, and guides designed to elevate your skills
+          </p>
+        </div>
+        <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {data.map((post) => (
+            <article key={post.id} className="flex flex-col items-start justify-between">
+              <div className="relative w-full">
+                <img
+                  src={post.thumbnail}
+                  alt=""
+                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                />
+                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+              </div>
+              <div className="max-w-xl">
+                <div className="mt-8 flex items-center gap-x-4 text-xs">
+                  <time dateTime={post.updateddate} className="text-gray-500">
+                    {post.updateddate}
+                  </time>
+                  <a
+                    href={post.category.href}
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    {post.category}
+                  </a>
+                </div>
+                <div className="group relative">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a href={post.href}>
+                      <span className="absolute inset-0" />
+                      {post.title}
+                    </a>
+                  </h3>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+                </div>
+                <div className="relative mt-8 flex items-center gap-x-4">
+                  <img src={post.user.imageUrlAuthor} alt="" className="h-10 w-10 rounded-full bg-gray-100" />
+                  <div className="text-sm leading-6">
+                    <p className="font-semibold text-gray-900">
+                      <a href={post.author.href}>
+                        <span className="absolute inset-0" />
+                        {post.user.username}
                       </a>
-                    </dd>
-                    <dt className="sr-only">Description</dt>
-                    <dd className="mt-2 w-full flex-none text-base leading-7 text-gray-600">{opening.description}</dd>
-                    <dt className="sr-only">Salary</dt>
-                    <dd className="mt-4 text-base font-semibold leading-7 text-gray-900">{opening.salary}$ Per Year</dd>
-                    <dt className="sr-only">Location</dt>
-                    <dd className="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
-                      <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 flex-none fill-gray-300" aria-hidden="true">
-                        <circle cx={1} cy={1} r={1} />
-                      </svg>
-                      {opening.location}
-                    </dd>
-                  </dl>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-8 flex border-t border-gray-100 pt-8">
-              <a href="https://finalproject-drab.vercel.app/jobs" className="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start your search now! <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
+                    </p>
+                    <p className="text-gray-600">{post.user.role}</p>
+                  </div>
+                </div>
+              </div>
+            </article>
+          ))}
         </div>
       </div>
     </div>
