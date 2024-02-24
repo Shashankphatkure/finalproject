fec2ba1a2bae72a5e013c870bda535dd6bb83044
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-25 03:02:15 +0530

    debug

--- skillconnect/app/recruiter/addjob/page.js
+++ skillconnect/app/recruiter/addjob/page.js
@@ -1,10 +1,3 @@
-import React from 'react'
-import Datadisplay1 from '@/components/data-displays/data-display1'
-import Testimonials1 from '@/components/testimonials/Testimonials1'
-import Cta1 from '@/components/CTA Sections/cta1'
-import Cta2 from '@/components/CTA Sections/cta2'
-import SectionHeader1 from '@/components/headers/section-header1'
-import Heroes3 from '@/components/heroes/Heroes3'
 import Addjobform from './addjobform'
 
 const page = () => {
