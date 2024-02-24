6dd8c8a6c56884c062e4ec1d09df9c5f13f48bc2
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-24 20:40:49 +0530

    updated course dashboard routes

--- skillconnect/app/course/dashboard/recommendations/page.js
+++ skillconnect/app/course/dashboard/recommendations/page.js
@@ -1,12 +1,16 @@
 import Coursecard from '@/components/Important-cards/Coursecard'
+import Coursecardenrolled from '@/components/Important-cards/Coursecardenrolled'
 import BlogSectionCourse from '@/components/blog-sections/BlogSectionCourse'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <BlogSectionCourse />
-      <Coursecard />
+     <main className="-mt-32">
+          <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+            <div className="rounded-lg bg-white px-5 py-6 shadow sm:px-6">{<Coursecardenrolled/>}</div>
+          </div>
+        </main>
     </div>
   )
 }
