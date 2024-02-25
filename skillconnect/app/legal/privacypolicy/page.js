2ba4769101c86c9e170549931358c142207b1251
Shashank Phatkure <shashankphatkure@gmail.com>
2024-02-26 01:14:13 +0530

    added privacy policy

---
+++ skillconnect/app/legal/privacypolicy/page.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="privacy-policy">
+      <h1>Privacy Policy</h1>
+      <p>
+        This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from [Your Website Name] (the “Site”).
+      </p>
+
+      <h2>Information We Collect</h2>
+      <p>
+        When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information.”
+      </p>
+      <p>
+        We collect Device Information using the following technologies:
+      </p>
+      <ul>
+        <li>“Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.</li>
+        <li>“Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</li>
+        <li>“Web beacons,” “tags,” and “pixels” are electronic files used to record information about how you browse the Site.</li>
+      </ul>
+
+      <h2>Information We Use</h2>
+      <p>
+        We use the information that we collect generally to fulfill any orders placed through the Site (including processing your payment and arranging for the delivery of your purchased course or job application), communicate with you, provide you with information or advertising relating to the Site, and administer the Site.
+      </p>
+
+      <h2>Sharing Your Personal Information</h2>
+      <p>
+        We share your Personal Information with third parties to help us use your Personal Information, as described above. For example, we use Shipping Carriers and other third parties to provide you with the products you have purchased. We also use third parties to help us fulfill and complete your orders, and to provide you with information or advertising relating to the Site.
+      </p>
+
+      <h2>Protecting Your Personal Information</h2>
+      <p>
+        We take reasonable measures to protect the Personal Information we collect from loss, theft, misuse, unauthorized access, disclosure, alteration, and destruction.
+      </p>
+
+      <h2>Changes to This Privacy Policy</h2>
+      <p>
+        We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
+      </p>
+
+      <h2>Your Consent</h2>
+      <p>
+        By using the Site, you consent to this Privacy Policy.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about this Privacy Policy, please contact us at [Your Email Address].
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
