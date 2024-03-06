97f389778f61bc751b597f48be9ef870358c9772
Shashank Phatkure <shashankphatkure@gmail.com>
2024-03-06 23:01:58 +0530

    updated admin

--- skillrecruiter/src/navigation/vertical/index.js
+++ skillrecruiter/src/navigation/vertical/index.js
@@ -83,33 +83,12 @@ const navigation = () => {
       icon: 'tabler:user',
       children: [
         {
-          title: 'List',
+          title: 'Add new Candidates',
           path: '/apps/user/list'
         },
         {
-          title: 'View',
-          children: [
-            {
-              title: 'Account',
-              path: '/apps/user/view/account'
-            },
-            {
-              title: 'Security',
-              path: '/apps/user/view/security'
-            },
-            {
-              title: 'Billing & Plans',
-              path: '/apps/user/view/billing-plan'
-            },
-            {
-              title: 'Notifications',
-              path: '/apps/user/view/notification'
-            },
-            {
-              title: 'Connection',
-              path: '/apps/user/view/connection'
-            }
-          ]
+          title: 'View Shortlisted',
+          path: '/apps/user/list'
         }
       ]
     },
