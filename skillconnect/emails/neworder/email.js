c77ee39fa1cc0cb68be7fcb45bcce7bed6c49ff9
Shashank Phatkure <shashankphatkure@gmail.com>
2024-04-21 01:40:47 +0530

    updated emails

--- skillconnect/emails/neworder/email.js
+++ skillconnect/emails/neworder/email.js
@@ -1,27 +1,27 @@
 const { createTransport } = require("nodemailer");
 
 const transporter = createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "shashankphatkurepro@gmail.com",
-      pass: "msdw hxkc zyns heej",
-    },
-  });
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "shashankphatkurepro@gmail.com",
+    pass: "msdw hxkc zyns heej",
+  },
+});
 
-  const mailOptions = {
-    from: "your_email@gmail.com",
-    to: "theayeshatkhan@gmail.com",
-    subject: "New Order Received",
-    text: "You have received a new order. Please check your dashboard for more details.",
-  };
+const mailOptions = {
+  from: "your_email@gmail.com",
+  to: "theayeshatkhan@gmail.com",
+  subject: "New Job Opportunities",
+  text: "You have received some new job. Please click to view more!",
+};
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email: ", error);
-    } else {
-      console.log("Email sent: ", info.response);
-    }
-  });
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error("Error sending email: ", error);
+  } else {
+    console.log("Email sent: ", info.response);
+  }
+});
